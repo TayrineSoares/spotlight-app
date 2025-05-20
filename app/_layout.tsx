b893751866eb321426fb53c853eb978c4430aec1
Tayrine Soares <tayrinecristina@hotmail.com>
2025-05-20 14:07:10 -0400

    Add safeareaview and safeareaprovider on layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex: 1}}>
+        <Stack>
+          <Stack.Screen name="index" options={{title: "Feed"}} />
+          <Stack.Screen 
+            name="notifications" 
+            options={{title: "Notifications" }} />
+
+
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+    )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,16 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 import { styles } from '../styles/auth.style';
+
+// import link from expo router 
+import { Link } from "expo-router";
+
 
 export default function Index() {
   return (
     <View style={styles.container} >
+      <Link href={"/notifications"}>Visit notifications screen </Link>
 
-      <Text style={styles.title}> Hello </Text>
-
-      {/* Pressable text */}
-      <TouchableOpacity onPress={() => alert("you touched")}> 
-        <Text>PRESS ME AAA</Text>
-      </TouchableOpacity>
-
-      {/* Image from local storage */}
-      <Image 
-        source={require("../assets/images/icon.png")}
-        style={{ 
-          width: 100, 
-          height: 100
-        }}
-      />
-
-      {/* Image from external font */}
-      <Image 
-        source={{
-          uri: "https://img.freepik.com/free-vector/pink-bloom-retro-nostalgic_24877-82517.jpg"
-          
-        }}
-        style={{ 
-          width: 100, 
-          height: 100, 
-          resizeMode:"cover"
-        }}
-      />
-
+      
      
     </View>
     
